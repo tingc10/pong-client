688affee12a0f722d56f7310dc0100313c0cbcad
Ting Chen <tingshen.chen@gmail.com>
2019-11-12 22:10:22 -0500

    Updates

---
+++ precache-manifest.3c2ad298fe3a8c3e667b7aa3fca96a13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5de6c9209c4d46641c25b68da6c3db2",
+    "url": "/pong-client/index.html"
+  },
+  {
+    "revision": "7e926688b3369f3649eb",
+    "url": "/pong-client/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "bb23a4dd5c0b7c0c0414",
+    "url": "/pong-client/static/js/2.651e1957.chunk.js"
+  },
+  {
+    "revision": "7e926688b3369f3649eb",
+    "url": "/pong-client/static/js/main.3487e177.chunk.js"
+  },
+  {
+    "revision": "f96fb1efe9c8f34b0727",
+    "url": "/pong-client/static/js/runtime-main.a28f8ede.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/pong-client/static/media/logo.25bf045c.svg"
+  }
+]);
